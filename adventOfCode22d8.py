084e8ff884113d8db7648c10e2e1fa9e081a1ec6
Kacper-W-Kozdon <k.kozdon@gmail.com>
2022-12-09 17:24:03 +0100

    day 8 full

--- adventOfCode22d8.py
+++ adventOfCode22d8.py
@@ -26,8 +26,9 @@ class TreeHouse:
         self.forest = np.array(load_files()).astype(int)
         self.visibleTrees = np.zeros(self.forest.shape)
         self.noVisibleTrees = 0
+        self.bestScore = 0
         
-    def __look_around(self, rowIdx, treeIdx):
+    def __look_around(self, rowIdx, treeIdx, mode = "scenicScore"):
         if self.forest.shape[0] != self.forest.shape[1]:
             return print("FOREST NOT IN A SQUARE GRID")
         visibilityTable = []
@@ -36,11 +37,33 @@ class TreeHouse:
             visibilityTable = [True]
             
         else:
-            north = all(self.forest[:rowIdx, treeIdx] < self.forest[rowIdx, treeIdx])
-            east = all(self.forest[rowIdx, treeIdx + 1:] < self.forest[rowIdx, treeIdx])
-            south = all(self.forest[rowIdx + 1:, treeIdx] < self.forest[rowIdx, treeIdx])
-            west = all(self.forest[rowIdx, :treeIdx] < self.forest[rowIdx, treeIdx])
-            visibilityTable += [north, east, south, west]
+            if mode == "lookAround":
+                north = all(self.forest[:rowIdx, treeIdx] < self.forest[rowIdx, treeIdx])
+                east = all(self.forest[rowIdx, treeIdx + 1:] < self.forest[rowIdx, treeIdx])
+                south = all(self.forest[rowIdx + 1:, treeIdx] < self.forest[rowIdx, treeIdx])
+                west = all(self.forest[rowIdx, :treeIdx] < self.forest[rowIdx, treeIdx])
+                visibilityTable += [north, east, south, west]
+            if mode == "scenicScore":
+                north = (self.forest[:rowIdx, treeIdx] < self.forest[rowIdx, treeIdx])[::-1]
+                east = (self.forest[rowIdx, treeIdx + 1:] < self.forest[rowIdx, treeIdx])
+                south = (self.forest[rowIdx + 1:, treeIdx] < self.forest[rowIdx, treeIdx])
+                west = (self.forest[rowIdx, :treeIdx] < self.forest[rowIdx, treeIdx])[::-1]
+                directionScores = []
+                directions = [north, east, south, west]
+                for direction in directions:
+                    score = 0                    
+                    for tree in direction:                      
+                        if tree == 0:
+                            score += 1
+                            break                        
+                        score += tree
+                    directionScores += [score]
+                northScore = directionScores[0]
+                eastScore = directionScores[1]
+                southScore = directionScores[2]
+                westScore = directionScores[3]
+                
+                visibilityTable += [northScore, eastScore, southScore, westScore]
             
         
         return visibilityTable
@@ -49,11 +72,20 @@ class TreeHouse:
     def __count_trees(self):
         self.noVisibleTrees = sum(self.visibleTrees.flatten())
         
-    def visible_trees(self):
+    def __best_score(self):
+        self.bestScore = max(self.visibleTrees.flatten())
+        
+    def visible_trees(self, mode = "lookAround"):
         for rowIdx, row in enumerate(self.forest):
             for treeIdx, tree in enumerate(row):
-                self.visibleTrees[rowIdx, treeIdx] = int(any(self.__look_around(rowIdx, treeIdx)))
-        self.__count_trees()
+                if mode == "lookAround":
+                    self.visibleTrees[rowIdx, treeIdx] = int(any(self.__look_around(rowIdx, treeIdx, mode)))
+                if mode == "scenicScore":
+                    self.visibleTrees[rowIdx, treeIdx] = int(np.prod(np.array(self.__look_around(rowIdx, treeIdx, mode))))
+        if mode == "lookAround":        
+            self.__count_trees()
+        if mode == "scenicScore":
+            self.__best_score()
         return self
     
 def run():
@@ -64,7 +96,7 @@ def run():
     # for row in treeHouse.visible_trees().visibleTrees:
     #     print(row)
         
-    return(treeHouse.visible_trees().noVisibleTrees)
+    return(treeHouse.visible_trees().noVisibleTrees, treeHouse.visible_trees("scenicScore").bestScore)
 
 print(run())
 # a = np.array([3, 7, 2])
