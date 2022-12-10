1c2ec18dcbf0c808596998295252ee621b48e211
Kacper-W-Kozdon <k.kozdon@gmail.com>
2022-12-10 16:34:10 +0100

    day 10 full (fun problem!)

--- adventOfCode22d10.py
+++ adventOfCode22d10.py
@@ -29,13 +29,12 @@ class Signal():
     def __init__(self):
         self.signal = load_files()
         self.signalStr = [1]
+        self.coursor = [1]
         self.totalStr = 0
         self.params = [20, 40]
         
     def find_str(self):
-        # print(len(self.signal))
-        # for line in self.signal:
-            # print(line[1])
+
         toAdd1 = 0
         toAdd2 = 0
         for lineIdx, line in enumerate(self.signal):
@@ -43,36 +42,35 @@ class Signal():
             
             if line[0] != "noop":
                 toAdd1 = line[1]
-                self.signalStr += [self.signalStr[-1]]
-                self.signalStr += [self.signalStr[-1] + line[1]]
+                self.coursor += [self.coursor[-1]]
+                self.coursor += [self.coursor[-1] + line[1]]
             if line[0] == "noop":
-                self.signalStr += [self.signalStr[-1]]
+                self.coursor += [self.coursor[-1]]
                 
-    
-        # self.signalStr.pop(0)
-        # self.signalStr.pop(0)
-        # print(self.signalStr)
-        # print(self.signalStr[220])
+        self.signalStr = self.coursor.copy()
+
         for lineIdx, line in enumerate(self.signalStr):
 
             self.signalStr[lineIdx] = line * (lineIdx + 1)
             
-        # print(self.signalStr)
-        # print(self.signalStr.index(21))
-        # print(self.signalStr[20])
-        # print(self.signalStr)
-        # print(self.signalStr[self.params[0] - 1::self.params[1]])
         self.totalStr = sum(self.signalStr[self.params[0] - 1::self.params[1]])
         return self
+    def printer(self):
+        for cycle, pix in enumerate(self.coursor):
+            crtLine = ["." for i in range(41)]
+            for i in [i - 1 for i in range(3)]:
+                crtLine[i + pix] = "#"
+            if cycle % 40 == 0:
+                print()
+            print(crtLine[cycle % 40], end = "")
+             
     
 def run():
     signal = Signal()
     signal.find_str()
     print(signal.totalStr)
+    signal.printer()
     return signal.totalStr
     
 print(run())
 
-# x = [2 * i for i in range(20)]
-# print(x)
-# print(x[2::5])
