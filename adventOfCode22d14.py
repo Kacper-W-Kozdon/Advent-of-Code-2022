3d744c35a4ad68821beed51fff72f626d517aa2e
Kacper-W-Kozdon <k.kozdon@gmail.com>
2022-12-18 20:06:24 +0100

    day 14 full

--- adventOfCode22d14.py
+++ adventOfCode22d14.py
@@ -2,7 +2,7 @@
 """
 Created on Sun Dec 18 17:41:44 2022
 
-@author: xBubblex
+oauthor: xBubblex
 """
 
 import re
@@ -27,6 +27,10 @@ class Stone:
         self.raw = load_files()
         self.lines = []
         self.board = np.array([])
+        self.maxH = 0
+        self.maxV = 0
+        self.minH = 0
+        self.minV = 0
         
     def __preprocess__(self):
         self.lines = []
@@ -49,7 +53,11 @@ class Stone:
         maxV = max(allCoords["ver"])
         minH = min(allCoords["hor"])
         minV = min(allCoords["ver"])
-        self.board = np.zeros((maxV + 2, maxH + 2), dtype = object)
+        self.maxH = maxH
+        self.maxV = maxV
+        self.minH = minH
+        self.minV = minV
+        self.board = np.zeros((maxV + 3, maxH + 2), dtype = object)
         return self
     
     def __prepare_stones__(self):
@@ -66,10 +74,20 @@ class Stone:
                 
         return self
     
-    def board_ready(self):
+    def board_ready(self, mode = "void"):
         self.__preprocess__()
         self.__prepare_board__()
         self.__prepare_stones__()
+        self.board[np.where(self.board == 0)] = "."
+        if mode == "noVoid":
+            # print(self.board.shape)
+            for counter in range(2 * self.maxV):
+                self.entry[1] += 1
+                self.board = np.insert(self.board, 0, ".", axis=1)
+                self.board = np.insert(self.board, -1, ".", axis=1)
+            self.board[-1, :] = "#"
+            # print(self.board.shape)
+            # print(self.board[-1, :])
         return self
     
 class Sand(Stone):
@@ -84,10 +102,10 @@ class Sand(Stone):
         canMove = True
         
         # print(self.grainCounter)
-        if self.board[prevPos[0], prevPos[1]] == "@":
+        if self.board[prevPos[0], prevPos[1]] == "o":
             return self
         
-        self.board[prevPos[0], prevPos[1]] = "@"
+        self.board[prevPos[0], prevPos[1]] = "o"
         
         
         while canMove:
@@ -95,28 +113,38 @@ class Sand(Stone):
             try:
                 self.board[prevPos[0], prevPos[1]]
                 
-                if self.board[prevPos[0] + 1, prevPos[1]] not in ["#", "@"]:
-                    self.board[prevPos[0] + 1, prevPos[1]] = "@"
+                if self.board[prevPos[0] + 1, prevPos[1]] not in ["#", "o"]:
+                    self.board[prevPos[0] + 1, prevPos[1]] = "o"
                     self.board[prevPos[0], prevPos[1]] = "."
                     prevPos = [prevPos[0] + 1, prevPos[1]]
-                elif self.board[prevPos[0] + 1, prevPos[1] - 1] not in ["#", "@"]:
-                    self.board[prevPos[0] + 1, prevPos[1] - 1] = "@"
+                elif self.board[prevPos[0] + 1, prevPos[1] - 1] not in ["#", "o"]:
+                    self.board[prevPos[0] + 1, prevPos[1] - 1] = "o"
                     self.board[prevPos[0], prevPos[1]] = "."
                     prevPos = [prevPos[0] + 1, prevPos[1] - 1]
-                elif self.board[prevPos[0] + 1, prevPos[1] + 1] not in ["#", "@"]:
-                    self.board[prevPos[0] + 1, prevPos[1] + 1] = "@"
+                elif self.board[prevPos[0] + 1, prevPos[1] + 1] not in ["#", "o"]:
+                    self.board[prevPos[0] + 1, prevPos[1] + 1] = "o"
                     self.board[prevPos[0], prevPos[1]] = "."
                     prevPos = [prevPos[0] + 1, prevPos[1] + 1]
                 else:
                     canMove = False
+                    if prevPos == self.entry:
+                        self.grainCounter += 1
+                        self.limReached = True
+                        return self
             except:
                 self.limReached = True
                 # print("BREAK")
                 return self
+            
         self.grainCounter += 1
+        # print(self.grainCounter)
         return self
         
-    def simulate_sand(self):
+    def simulate_sand(self, mode = "void"):
+        self.board_ready(mode)
+        if mode == "noVoid":
+            entry = [0, 500 + self.maxH]
+        
         self.grainCounter = 0
         self.limReached = False
         while not self.limReached:
@@ -124,14 +152,21 @@ class Sand(Stone):
         
         return self
     
+    def print_sand(self, rangeView = 20):
+        for line in self.board:
+            for symbol in line[self.entry[1] - rangeView : self.entry[1] + rangeView]:
+                print(symbol, end = "")
+            print()
+        print()
+    
 def run():
     stonePath = Stone()
     stonePath.board_ready()
     sand = Sand()
     # for line in sand.board_ready().board:
-        # print(line[494 : 504])
-    grainNo = sand.board_ready().simulate_sand().grainCounter
-    print()
+        # print(line[490 : 520])
+    grainNo = sand.simulate_sand(mode = "noVoid").grainCounter
+    # sand.print_sand()
     return grainNo
 print(run())
 
