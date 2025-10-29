7ce22e0e542648e5bcc1892a93ef6f0cc34eb5b7
Kacper-W-Kozdon <k.kozdon@gmail.com>
2025-10-29 20:40:05 +0100

    day 16 2nd attempt at clean code

--- adventOfCode22d16attempt2.py
+++ adventOfCode22d16attempt2.py
@@ -49,26 +49,35 @@ class Valve:
     time_turned_on: int = -1
 
 
+def compute_test_value(total_flow: int, time_remaining: int, flow_valves: list[Valve], path: list[Valve], total_flow: int) -> Union[dict[str, int], None]:
+    
+    available_valves = [valve for valve in flow_valves if valve not in path]
+    
+    for test_valve in available_valves:
+        test_value = (time_remaining - 1) * (total_flow + test_valve.flow_rate)
+
+
+    return NotImplementedError
+
+
 @dataclass
 class Flow_State:
     current_valve: str = "AA"
     paths_taken: list = field(default_factory=list)
     total_flow: int = 0
-    summed_flow: int = 0
     time_elapsed: int = 0
     distances_dict: dict[str, int] = field(default_factory=dict)
     flow_valves: list = field(default_factory=list)
-    valves: list = field(default_factory=list)
+    valves: dict = field(default_factory=dict)
     time_available: int = 30
+    non_zero_flow_valves: list = field(default_factory=list)
 
     def update_state(self):
         if self.current_valve == "AA":
-            non_zero_flow_valves: list[Valve] = [valve for valve in self.valves if getattr(valve, "flow_rate") > 0]
-            self.time_available -= len(non_zero_flow_valves)
+            self.non_zero_flow_valves = [valve for valve in self.valves if getattr(valve, "flow_rate") > 0]
+            self.distances_dict = compute_distances(self.valves)
 
-        return NotImplementedError
-
-    def compute_total_maximum(self) -> Union[int, type]:
+        
 
         return NotImplementedError
 
@@ -127,14 +136,12 @@ def compute_distances(valves: dict[str, Valve], *args, **kwargs) -> dict[str, in
         for end_valve_idx in range(start_valve_idx + 1, num_valves):
             end_valve_name = list(valves.keys())[end_valve_idx]
             distance = pair_distance(valves, start_valve_name, end_valve_name)
-            distances_dict[f"{start_valve_name}:{end_valve_name}"] = distance
+            distances_dict[f"{start_valve_name}:{end_valve_name}"], distances_dict[f"{end_valve_name}:{start_valve_name}"] = distance, distance
 
     # print(list(distances_dict.items()))
     print(f"{len(list(distances_dict.items()))=}")
     print(f"{len(list(non_zero_flow_valves.items()))=}")
     return distances_dict
-
-
 
 
 def main() -> None:
@@ -144,9 +151,7 @@ def main() -> None:
     valves_list = list(valves.items())
     valves_num = len(valves_list)
 
-    distances_dict = compute_distances(valves)
-
-    state = Flow_State(distances_dict=distances_dict)
+    state = Flow_State(valves=valves)
 
 
 
